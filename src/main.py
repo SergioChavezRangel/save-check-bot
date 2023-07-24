20ab0afcf64d69c720be55171d7bad264ef7e04e
Sergio Chavez <sergio@simplecitizen.com>
2023-07-24 10:36:43 -0600

    Set up new project

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-# This is a sample Python script.
+
 import subprocess
 from datetime import timedelta, datetime
 
 
-# Press ⌃R to execute it or replace it with your code.
+# Press ⌃R to execute it.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
@@ -14,13 +14,14 @@ def print_hi(name):
 
 def get_business_days(start_date, end_date):
     # generating dates
+    total_elapsed_days = (end_date - start_date).days
     dates = (start_date + timedelta(idx + 1)
-             for idx in range((end_date - start_date).days))
+             for idx in range(total_elapsed_days))
 
     # summing all weekdays
     res = sum(1 for day in dates if day.weekday() < 5)
 
-    return res
+    return res, total_elapsed_days
 
 
 # Press the green button in the gutter to run the script.
@@ -30,6 +31,6 @@ if __name__ == '__main__':
     out = p.stdout.read()
     print(out)
     startDate = datetime(2023, 6, 28)
-    print("Total business days since " + startDate.strftime("%m/%d/%Y") + ": " + str(get_business_days(startDate, datetime.now())))
+    business_days, elapsed_days = get_business_days(startDate, datetime.now())
+    print("Total business days since " + startDate.strftime("%m/%d/%Y") + " (" + str(elapsed_days) + "): " + str(business_days))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
